9712f5c664af7e7506238a45d474e40ca3846076
PrasadPrabash <prasad.lakman@gmail.com>
2023-10-02 11:07:21 +0530

    Successfully get the output

--- ClimbingStairs.java
+++ ClimbingStairs.java
@@ -1,14 +1,27 @@
 public class ClimbingStairs {
     public static void main(String[] args) {
 
-        int n = 4; // The target number to reach
+        int n = 45; // The target number to reach
 
-        int ways = countWays(n);
-        System.out.println("Number of ways to get " + n + " using 1s and 2s: " + ways);
+        if(n==1) System.out.println("1");
+        
+        if(n==2) System.out.println("2");
+
+        int[] a =  new int[n];
+        a[0]=1;
+        a[1]=2;
+
+        for(int i=2;i<n;i++){
+            a[i]=a[i-1]+a[i-2];
+        }
+        System.out.println(a[n-1]); 
+
+        // int ways = countWays(n);
+        // System.out.println("Number of ways to get " + n + " using 1s and 2s: " + ways);
 
         
     }
-    public static int countWays(int n) {
+    /* public static int countWays(int n) {
         if (n <= 1) {
             return 1; // Only one way to make 0 or 1 using 1s and 2s (no operation or using 1 once)
         }
@@ -18,6 +31,6 @@ public class ClimbingStairs {
         int ways2 = countWays(n - 2); // Subtract 2 from n
 
         return ways1 + ways2;
-    }
+    } */
     
 }
