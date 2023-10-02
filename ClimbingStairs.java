6e2cede5e7134eb3a7b9e24253f5a7ce97a4f3fc
PrasadPrabash <prasad.lakman@gmail.com>
2023-10-02 10:50:08 +0530

    Using recursion to count the ways

--- ClimbingStairs.java
+++ ClimbingStairs.java
@@ -1,3 +1,25 @@
 public class ClimbingStairs {
+    public static void main(String[] args) {
+
+        int n = 5; // The target number to reach
+
+        int ways = countWays(n);
+        System.out.println("Number of ways to get " + n + " using 1s and 2s: " + ways);
+
+        
+    }
+    public static int countWays(int n) {
+        if (n <= 1) {
+            return 1; // Only one way to make 0 or 1 using 1s and 2s (no operation or using 1 once)
+        }
+
+        // Use recursion to count the ways
+        int ways1 = countWays(n - 1); // Subtract 1 from n
+        int ways2 = countWays(n - 2); // Subtract 2 from n
+
+        return ways1 + ways2;
+    }
+
+
     
 }
